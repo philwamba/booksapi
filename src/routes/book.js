45152208220a90d2dd1820e79d178800e51481d6
Phil Wamba <philwamba@gmail.com>
2020-02-11 10:29:42 +0300

    Add books PUT request

--- src/routes/book.js
+++ src/routes/book.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-param-reassign */
 const express = require('express');
 const Book = require('../models/bookModel');
 
 const bookRouter = express.Router();
 
-bookRouter.post('/books', (req, res) => {  
+bookRouter.post('/books', (req, res) => {
   const book = new Book(req.body);
   book.save();
   return res.status(201).json(book);
@@ -24,16 +25,33 @@ bookRouter.get('/books', (req, res) => {
     if (!error) {
       return res.json(books);
     }
-    return res.json({ 
-      error, 
+    return res.json({
+      error,
     });
   });
 });
-  
 
 bookRouter.get('/books/:bookId', (req, res) => {
   Book.findById(req.params.bookId, (err, book) => {
     if (!err) {
+      return res.json(book);
+    }
+    return res.json({ error: err });
+  });
+});
+
+bookRouter.put('/books/:bookId', (req, res) => {
+  Book.findById(req.params.bookId, (err, book) => {
+    if (!err) {
+      book.author = req.body.author;
+      book.country = req.body.country;
+      book.imageLink = req.body.imageLink;
+      book.language = req.body.language;
+      book.link = req.body.link;
+      book.pages = req.body.pages;
+      book.title = req.body.title;
+      book.year = req.body.year;
+      book.save();
       return res.json(book);
     }
     return res.json({ error: err });
